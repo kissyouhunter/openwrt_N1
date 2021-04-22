f0d7429f0b65de864a3fe31085a791c31317f508
kissyouhunter <63334206+kissyouhunter@users.noreply.github.com>
2021-04-23 04:12:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,8 @@
 sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
 #修改主机名
 sed -i 's/OpenWrt/N1/g' package/base-files/files/bin/config_generate
+#修改build日期
+sed -i "s/R21.4.18/R21.4.18 2021.04.23 powered by kissyouhunter/g" package/lean/default-settings/files/zzz-default-settings
 #themes
 git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly package/luci-theme-Butterfly
 git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark package/luci-theme-Butterfly-dark
