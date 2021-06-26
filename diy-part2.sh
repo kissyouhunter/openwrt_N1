ab8c1b794de426134debdc23a60d26b88bb476ad
kissyouhunter <63334206+kissyouhunter@users.noreply.github.com>
2021-06-26 20:49:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,18 +12,18 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.3/g' package/base-files/files/bin/config_generate
 #修改主机名
 sed -i 's/OpenWrt/N1/g' package/base-files/files/bin/config_generate
 # 添加旁路由防火墙
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 #修改build日期
-sed -i "s/R21.4.18/R21.4.18 2021.04.30 powered by kissyouhunter/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/Openwrt/N1/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/R21.6.22/R21.6.22 2021.06.27 powered by kissyouhunter/g" package/lean/default-settings/files/zzz-default-settings
+#sed -i "s/Openwrt/N1/g" package/lean/default-settings/files/zzz-default-settings
 #修改luci-app-adguardhome配置config文件
-sed -i 's'/usr/bin/AdGuardHome'/'usr/bin/AdGuardHome/AdGuardHome'/g' feeds/kenzok/luci-app-adguardhome/root/etc/config/AdGuardHome
+#sed -i 's'/usr/bin/AdGuardHome'/'usr/bin/AdGuardHome/AdGuardHome'/g' feeds/kenzok/luci-app-adguardhome/root/etc/config/AdGuardHome
 #删除默认密码
 #sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 #themes
-git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly package/luci-theme-Butterfly
-git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark package/luci-theme-Butterfly-dark
+#git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly package/luci-theme-Butterfly
+#git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark package/luci-theme-Butterfly-dark
