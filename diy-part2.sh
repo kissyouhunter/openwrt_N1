f24bd000d24341bc478966ea3683cf487c926f73
kissyouhunter <63334206+kissyouhunter@users.noreply.github.com>
2021-06-26 20:50:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@ sed -i 's/192.168.1.1/192.168.2.3/g' package/base-files/files/bin/config_generat
 #修改主机名
 sed -i 's/OpenWrt/N1/g' package/base-files/files/bin/config_generate
 # 添加旁路由防火墙
-echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+echo "#iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 #修改build日期
 sed -i "s/R21.6.22/R21.6.22 2021.06.27 powered by kissyouhunter/g" package/lean/default-settings/files/zzz-default-settings
 #sed -i "s/Openwrt/N1/g" package/lean/default-settings/files/zzz-default-settings
